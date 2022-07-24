32cd18fea32021f8a0b66437921448252f0839cb
Shannon Gurule <shannon.gurule@gmail.com>
2022-07-24 10:50:38 -0400

    Updates

---
+++ precache-manifest.baee0904c50c8eb0e7fc9c84a58f78f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e80b90f4a808097ab18e",
+    "url": "/todoapp/static/css/main.1008c0f2.chunk.css"
+  },
+  {
+    "revision": "e80b90f4a808097ab18e",
+    "url": "/todoapp/static/js/main.e80b90f4.chunk.js"
+  },
+  {
+    "revision": "b27d6e33a5d6b3dae832",
+    "url": "/todoapp/static/js/1.b27d6e33.chunk.js"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "2201210f491343db5c468a5a70ab6761",
+    "url": "/todoapp/index.html"
+  }
+];
